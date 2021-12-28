5bae41baa32aa23bb76f81e3c0b27ca79fe61909
Антон Шмидт <schmidt.ajax@gmail.com>
2021-12-28 16:25:08 +0700

    Solution: BFS

--- main.cpp
+++ main.cpp
@@ -5,9 +5,62 @@
 using namespace std;
 
 class Solution {
+  enum class Tag {
+    Root, LeftChild, RightChild
+  };
+
+  bool isSameNodes(const pair<TreeNode *, Tag> &l, const pair<TreeNode *, Tag> &r) {
+    return l.first->val == r.first->val
+        && l.second == r.second;
+  }
+
  public:
-  bool isSameTree(TreeNode* p, TreeNode* q) {
-    // FIXME
+  bool isSameTree(TreeNode *p, TreeNode *q) {
+    // Both empty trees are same
+    if (!p && !q) {
+      return true;
+    }
+
+    if (p && q) {
+      queue<pair<TreeNode *, Tag>> queue_left, queue_right;
+      queue_left.emplace(p, Tag::Root);
+      queue_right.emplace(q, Tag::Root);
+      while (not queue_left.empty()) {
+        const size_t count = queue_left.size();
+        if (count != queue_right.size()) {
+          return false;
+        }
+        for (size_t i = 0; i < count; ++i) {
+          auto l = queue_left.front();
+          auto r = queue_right.front();
+          if (not isSameNodes(l, r)) {
+            return false;
+          }
+          // Enqueue left tree next level nodes
+          TreeNode *node_left = l.first;
+          if (node_left->left) {
+            queue_left.emplace(node_left->left, Tag::LeftChild);
+          }
+          if (node_left->right) {
+            queue_left.emplace(node_left->right, Tag::RightChild);
+          }
+          queue_left.pop();
+
+          // Enqueue right tree next level nodes
+          TreeNode *node_right = r.first;
+          if (node_right->left) {
+            queue_right.emplace(node_right->left, Tag::LeftChild);
+          }
+          if (node_right->right) {
+            queue_right.emplace(node_right->right, Tag::RightChild);
+          }
+          queue_right.pop();
+        }
+      }
+
+      return queue_right.empty();
+    }
+
     return false;
   }
 };
@@ -15,18 +68,28 @@ class Solution {
 void TestIsSameTree() {
   Solution s;
   {
-    TreeNode* a = stringToTreeNode("[1,2,3]"s);
-    TreeNode* b = stringToTreeNode("[1,2,3]"s);
+    TreeNode *a = stringToTreeNode("[1,2,3]"s);
+    TreeNode *b = stringToTreeNode("[1,2,3]"s);
     assert(s.isSameTree(a, b));
   }
   {
-    TreeNode* a = stringToTreeNode("[1,2]"s);
-    TreeNode* b = stringToTreeNode("[1,null,3]"s);
+    TreeNode *a = stringToTreeNode("[1,2]"s);
+    TreeNode *b = stringToTreeNode("[1,null,3]"s);
     assert(not s.isSameTree(a, b));
   }
   {
-    TreeNode* a = stringToTreeNode("[1,2,1]"s);
-    TreeNode* b = stringToTreeNode("[1,1,2]"s);
+    TreeNode *a = stringToTreeNode("[1,2,1]"s);
+    TreeNode *b = stringToTreeNode("[1,1,2]"s);
+    assert(not s.isSameTree(a, b));
+  }
+  {
+    TreeNode *a = stringToTreeNode("[]"s);
+    TreeNode *b = stringToTreeNode("[]"s);
+    assert(s.isSameTree(a, b));
+  }
+  {
+    TreeNode *a = stringToTreeNode("[1]"s);
+    TreeNode *b = stringToTreeNode("[1,null,2]"s);
     assert(not s.isSameTree(a, b));
   }
 }
